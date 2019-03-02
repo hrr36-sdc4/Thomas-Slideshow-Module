63fbb11719776a9bf835b202d3bc4a3a922f509a
Jake Ascher <ascherj@gmail.com>
2019-03-02 17:45:04 -0500

    Create data generation shell script
    This script generates 10M listing records and 50M-100M image records.

---
+++ db/generation/generate.sh
@@ -0,0 +1,17 @@
+echo "Running script..."
+echo "Generating listing records..."
+rm ./listing_records/*
+node ./generate_listings.js
+echo "10M records saved."
+echo "Generating image records..."
+rm ./image_records/*
+for i in {0..99}; do
+  # generate_images.js generates image records for 100000 listings at a time
+  # 5-10 image records are created for each listing record
+  # the script creates 10 CSV files each w/ data for 10000 listings (10 * 10000 = 100000)
+  # exeucte the script 100 times to generate data for 10M listings (1000 CSV files total)
+  # total number of image records should be between 50M and 100M
+  node ./generate_images.js $((i * 100000)) $((i * 10))
+  echo "$(((i + 1) * 100000)) records saved."
+done
+echo "Data generation complete."
