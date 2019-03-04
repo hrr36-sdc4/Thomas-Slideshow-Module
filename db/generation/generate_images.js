3209752859189182bd676c1856f0f829542e6409
Jake Ascher <ascherj@gmail.com>
2019-03-04 18:23:56 -0500

    Read/save currentId from/to file

--- db/generation/generate_images.js
+++ db/generation/generate_images.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const faker = require('faker');
-const { getFileNames, stringify, saveRecords } = require('./utils');
+const { getCurrentId, getFileNames, stringify, saveRecords, saveCurrentId } = require('./utils');
 
 let imageFilenames = [];
 const args = process.argv.slice(2).map(Number);
@@ -17,30 +17,39 @@ const generateImageRecord = (id, listingId, index) => {
 const generateImageRecords = (n, startingId, fileNum) => {
   let images = [];
   let remaining = n;
-  let currentId = 0;
+  let currentId;
   let currentListingId = startingId;
   let counter = fileNum;
 
-  getFileNames(path.join(__dirname, 'stock_photos'))
-    .then((fileNames) => {
-      imageFilenames = fileNames;
-      while (remaining) {
-        for (let i = 0; i < 10000; i++) {
-          // generate 5-10 images per listing
-          const randomAmount = 5 + Math.floor(Math.random() * 6);
-          for (let j = 0; j < randomAmount; j++) {
-            images.push(generateImageRecord(currentId, currentListingId, j));
-            currentId += 1;
+  getCurrentId()
+    .then((id) => {
+      currentId = id;
+    })
+    .then(() => {
+      getFileNames(path.join(__dirname, 'stock_photos'))
+        .then((fileNames) => {
+          imageFilenames = fileNames;
+          while (remaining) {
+            for (let i = 0; i < 10000; i++) {
+              // generate 5-10 images per listing
+              const randomAmount = 5 + Math.floor(Math.random() * 6);
+              for (let j = 0; j < randomAmount; j++) {
+                images.push(generateImageRecord(currentId, currentListingId, j));
+                currentId += 1;
+              }
+              currentListingId += 1;
+              remaining -= 1;
+            }
+            const stringifiedImages = stringify(['id', 'listingId', 'index', 'url', 'description'], images);
+            saveRecords(`./image_records/images${counter}.csv`, stringifiedImages);
+            counter += 1;
+            images = [];
           }
-          currentListingId += 1;
-          remaining -= 1;
-        }
-
-        const stringifiedImages = stringify(['id', 'listingId', 'index', 'url', 'description'], images);
-        saveRecords(`./image_records/images${counter}.csv`, stringifiedImages);
-        counter += 1;
-        images = [];
-      }
+          saveCurrentId(currentId);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     })
     .catch((err) => {
       console.error(err);
