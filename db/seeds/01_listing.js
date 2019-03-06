0664054ada4d5b90bdd6738ba66180bdc6218923
Jake Ascher <ascherj@gmail.com>
2019-03-05 20:06:41 -0500

    Seed 10 million listing records

--- db/seeds/01_listing.js
+++ db/seeds/01_listing.js
@@ -1,12 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 
-exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
+const getRecords = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, filePath), 'utf8', (err, fileData) => {
+      if (err) {
+        reject(err);
+      } else {
+        const records = fileData.split('\n');
+        resolve(records);
+      }
+    });
+  });
+};
+
+const prepareListings = (knex, fileNum) => {
+  return getRecords(`../generation/listing_records/listings${fileNum}.csv`)
+    .then((listings) => {
+      const formattedRows = listings.slice(1, -1).map(listing => ({ id: listing }));
+      return knex.batchInsert('listing', formattedRows, 10000);
+    });
+};
+
+exports.seed = (knex) => {
   return knex('listing').del()
     .then(() => {
-      // Inserts seed entries
-      return knex('listing').insert([
-        { id: 1 },
-        { id: 2 },
-      ]);
+      const listingPromises = [];
+      for (let i = 0; i <= 9; i++) {
+        listingPromises.push(prepareListings(knex, i));
+      }
+      return Promise.all(listingPromises);
     });
 };
