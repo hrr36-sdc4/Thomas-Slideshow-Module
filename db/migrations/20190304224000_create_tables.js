d9e4b801cf48d87ff5060982b53e459ca537a2eb
Jake Ascher <ascherj@gmail.com>
2019-03-05 20:05:12 -0500

    Update migration file

--- db/migrations/20190304224000_create_tables.js
+++ db/migrations/20190304224000_create_tables.js
@@ -5,7 +5,7 @@ exports.up = function (knex, Promise) {
   }).then(() => {
     return knex.schema.createTable('image', (table) => {
       table.integer('id').primary();
-      table.integer('listing').references('id').inTable('listing');
+      table.integer('listing');
       table.integer('index');
       table.string('url');
       table.string('description');
@@ -15,6 +15,6 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema
-    .dropTableIfExists('listing')
-    .dropTableIfExists('image');
+  .dropTableIfExists('image')
+  .dropTableIfExists('listing');
 };
