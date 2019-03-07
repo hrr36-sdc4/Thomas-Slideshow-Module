5d2483c5c452a58547b78ad0c9ee2abb9845db31
Jake Ascher <ascherj@gmail.com>
2019-03-06 19:29:44 -0500

    Seed ~75 million image records

--- db/seeds/02_image.js
+++ db/seeds/02_image.js
@@ -1,11 +1,39 @@
+const { getRecords } = require('../seed_utils');
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+
+const prepareImages = (fileNum) => {
+  const formattedRows = [];
+  return getRecords(`./generation/image_records/images${fileNum}.csv`)
+    .then((images) => {
+      images.slice(1, -1).forEach((image) => {
+        const fields = image.split(',');
+        const formattedRow = {
+          id: Number(fields[0]),
+          listing: Number(fields[1]),
+          index: Number(fields[2]),
+          url: fields[3],
+          description: fields[4],
+        };
+        formattedRows.push(formattedRow);
+      });
+      return formattedRows;
+    });
+};
+
+const seedImages = (knex, fileNum) => {
+  return prepareImages(fileNum).then((rows) => {
+    return knex.batchInsert('image', rows, 10000);
+  });
+};
+
+exports.seed = (knex) => {
   return knex('image').del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('image').insert([
-        { id: 1, listing: '1', index: '0', url: 'http://google.com', description: 'asdf' },
-      ]);
+    .then(async () => {
+      let currentFileNum = 0;
+      while (currentFileNum < 1000) {
+        await seedImages(knex, currentFileNum);
+        console.log('seeded image file:', currentFileNum);
+        currentFileNum += 1;
+      }
     });
 };
