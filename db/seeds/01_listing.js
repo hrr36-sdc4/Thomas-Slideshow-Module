0515b423ebf890ef5eb6fd383f593c19217860be
Jake Ascher <ascherj@gmail.com>
2019-03-06 19:28:12 -0500

    Refactor 01_listing.js

--- db/seeds/01_listing.js
+++ db/seeds/01_listing.js
@@ -1,33 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const { getRecords } = require('../seed_utils');
 
-const getRecords = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, filePath), 'utf8', (err, fileData) => {
-      if (err) {
-        reject(err);
-      } else {
-        const records = fileData.split('\n');
-        resolve(records);
-      }
-    });
-  });
-};
-
-const prepareListings = (knex, fileNum) => {
-  return getRecords(`../generation/listing_records/listings${fileNum}.csv`)
+const seedListings = (knex, fileNum) => {
+  return getRecords(`./generation/listing_records/listings${fileNum}.csv`)
     .then((listings) => {
-      const formattedRows = listings.slice(1, -1).map(listing => ({ id: listing }));
-      return knex.batchInsert('listing', formattedRows, 10000);
+      const formattedRows = listings.slice(1, -1).map(listing => ({ id: Number(listing) }));
+      console.log('preparing listing file:', fileNum);
+      return knex.batchInsert('listing', formattedRows, 10000).then(() => {
+        console.log('seeded listing file:', fileNum);
+      });
     });
 };
+
 
 exports.seed = (knex) => {
   return knex('listing').del()
     .then(() => {
       const listingPromises = [];
       for (let i = 0; i <= 9; i++) {
-        listingPromises.push(prepareListings(knex, i));
+        listingPromises.push(seedListings(knex, i));
       }
       return Promise.all(listingPromises);
     });
