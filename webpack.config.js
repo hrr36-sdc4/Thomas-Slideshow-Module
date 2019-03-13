b27442eb6f18afc3bdcd6e40184f9a63368d31d7
Jake Ascher <ascherj@gmail.com>
2019-03-13 19:19:08 -0400

    Move client/dist/ files into public/ and update paths accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,53 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
+// const common = {
+//   context: SRC_DIR,
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           {
+//             loader: 'style-loader'
+//           },
+//           {
+//             loader: 'css-loader'
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           {
+//             loader: 'style-loader'
+//           },
+//           {
+//             loader: 'css-loader'
+//           },
+//           {
+//             loader: 'sass-loader'
+//           }
+//         ]
+//       }
+//     ]
+//   },
+// };
+
 module.exports = {
-  entry: __dirname + `/client/src/index.jsx`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: __dirname + `/client/dist`
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -44,3 +89,26 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   }
 };
+
+// const client = {
+//   entry: './client.js',
+//   output: {
+//     path: DIST_DIR,
+//     filename: 'app.js',
+//   },
+// };
+
+// const server = {
+//   entry: './server.js',
+//   target: 'node',
+//   output: {
+//     path: DIST_DIR,
+//     filename: 'app-server.js',
+//     libraryTarget: 'commonjs-module'
+//   }
+// };
+
+// module.exports = [
+//   Object.assign({}, common, client),
+//   Object.assign({}, common, server),
+// ];
